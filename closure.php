0d02a219f0d168c8d1c73dd641223eace99c6039
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2014-09-02 12:47:43 +0700

    Contoh penggunaan closure untuk menerima variable dari luar fungsi

--- closure.php
+++ closure.php
@@ -2,12 +2,12 @@
 $title = "Mas";
 $nama = "Rahmat Awaludin";
 
-function namaAnda($title, $nama) {
-    return $title . ' ' . $nama;
-}
-
 function bisa($nama) {
     echo "Anda pasti bisa Closure, $nama!\n";
 }
 
-bisa(namaAnda($title, $nama));
+$nama = function() use ($title, $nama) {
+    return $title . ' ' . $nama;
+};
+
+bisa($nama());
